c2bdbf3b3ff07639d060e2b12cbba7f2f7bd2a2e
Abdulhannan146 <109844501+Abdulhannan146@users.noreply.github.com>
2023-03-27 12:19:25 +0500

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,19 @@
+const openMenu = () => {
+    document.querySelector('.backdrop').classList.add('active');
+    document.querySelector('aside').classList.add('active');
+  };
+  
+  const closeMenu = () => {
+    document.querySelector('.backdrop').classList.remove('active');
+    document.querySelector('aside').classList.remove('active');
+  };
+  
+  document.getElementById('menuBtn').addEventListener('click', e => {
+    e.preventDefault();
+    openMenu();
+  });
+  
+  document.querySelector('aside button.close').addEventListener('click', e => {
+    closeMenu();
+  });
+  
